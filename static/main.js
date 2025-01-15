4a5f292a7b7d87391b7b6bedeed24977f2a6be61
Axel Wasserman <axelwas07@gmail.com>
2025-01-15 10:33:03 +0100

    basic solidjs working

---
+++ static/main.js
@@ -0,0 +1,69 @@
+// @ts-check
+'use strict';
+
+import { createSignal, batch, For } from "https://esm.sh/solid-js@1.8.1";
+import { createStore } from "https://esm.sh/solid-js@1.8.1/store"
+import { render } from "https://esm.sh/solid-js@1.8.1/web";
+import html from "https://esm.sh/solid-js@1.8.1/html";
+
+
+const [getPosts, setPosts] = createSignal([]);
+
+const InfoColumn = () => {
+    return html`
+        <div class="column-width">
+            <span style="text-align: center;">TODO</span>
+        </div>
+    `;
+};
+
+const WritePostColumn = () => {
+    const [waah, setWaah] = createSignal(0);
+    const onClick = () => {
+        setWaah(waah() + 1);
+    };
+
+    const pleepString = () => {
+        if (waah() === 0) {
+            return "Pleep!";
+        }
+        return `Pleeped ${waah()}`;
+    };
+
+    return html`
+        <div class="flex-down column-width">
+            <textarea class="post-textarea" placeholder="What's on your mind?"></textarea>
+            <div class="flex-right space-between" style="margin-top: 6px">
+                <div></div>
+                <button class="pleep-button" onmousedown=${onClick}>${pleepString}</button>
+            </div>
+        </div>
+    `;
+};
+
+const PostsColumn = () => {
+    return html`
+        <div class="flex-down column-width">
+
+        </div>
+    `
+};
+
+const App = () => {
+    return html`
+        <div class="flex-right space-around">
+            <div class="flex-right column-container">
+                ${WritePostColumn}
+                ${PostsColumn}
+                ${InfoColumn}
+            </div>
+        </div>
+    `;
+};
+
+const root = document.getElementById("solidjs-root");
+if (root === null) {
+    alert("Invalid html from server");
+} else {
+    render(App, root);
+}
