b7bd8dfe3474a5e18520afff2e5dede1f32fd245
Axel Wasserman <axelwas07@gmail.com>
2025-01-16 11:23:37 +0100

    unfinished

--- static/main.js
+++ static/main.js
@@ -1,13 +1,10 @@
 // @ts-check
-'use strict';
-
 import { createSignal, batch, For } from "https://esm.sh/solid-js@1.8.1";
 import { createStore } from "https://esm.sh/solid-js@1.8.1/store"
 import { render } from "https://esm.sh/solid-js@1.8.1/web";
 import html from "https://esm.sh/solid-js@1.8.1/html";
 
-
-const [getPosts, setPosts] = createSignal([]);
+const [getPosts, setPosts] = createSignal(/** @type {string[]} */ ([]));
 
 const InfoColumn = () => {
     return html`
@@ -19,24 +16,52 @@ const InfoColumn = () => {
 
 const WritePostColumn = () => {
     const [waah, setWaah] = createSignal(0);
+    /**
+     * @type {undefined | HTMLTextAreaElement}
+     */
+    let textAreaRef = undefined;
     const onClick = () => {
         setWaah(waah() + 1);
+        if (textAreaRef !== undefined) {
+            const posts = getPosts();
+            posts.push(textAreaRef?.value);
+            setPosts(posts);
+        }
     };
 
     const pleepString = () => {
         if (waah() === 0) {
             return "Pleep!";
         }
-        return `Pleeped ${waah()}`;
+        return `Pleeped ${waah()} times and ${textAreaRef?.value}`;
     };
 
     return html`
         <div class="flex-down column-width">
-            <textarea class="post-textarea" placeholder="What's on your mind?"></textarea>
+            <textarea class="post-textarea" placeholder="What's on your mind?" ref=${
+                /**
+                 * @param {HTMLTextAreaElement} el  
+                 */
+                (el) => (textAreaRef = el)
+            }></textarea>
             <div class="flex-right space-between" style="margin-top: 6px">
                 <div></div>
                 <button class="pleep-button" onmousedown=${onClick}>${pleepString}</button>
             </div>
+        </div>
+    `;
+};
+
+
+
+/**
+ * @typedef {{postText: string}} PostParam
+ * @param {PostParam} 
+ */
+const Post = (props) => {
+    return html`
+        <div>
+            <span>${props.}</span>
         </div>
     `;
 };
