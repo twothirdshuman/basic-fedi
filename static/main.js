db536ce3f81adc6cbb1c223ca0f054ac6cd942b3
Axel Wasserman <axelwas07@gmail.com>
2025-02-19 14:36:41 +0100

    some new stuff idk

--- static/main.js
+++ static/main.js
@@ -3,16 +3,11 @@ import { createSignal, batch, For } from "https://esm.sh/solid-js@1.8.1";
 // import { createStore } from "https://esm.sh/solid-js@1.8.1/store"
 import { render } from "https://esm.sh/solid-js@1.8.1/web";
 import html from "https://esm.sh/solid-js@1.8.1/html";
-
-const [getPosts, setPosts] = createSignal(["Mamma mia!", "wow solidjs doesn't suck balls", 
-    "you can only kiss yourself on the lips in a mirror",
-    "the mening of life is uifhiousfhoasiduf",
-    "your mom! HAHAHAHA got youuuuu!",
-    "WEEEWOOO WEEEWOOO WEEEWOOO",
-    "I am happy and so exicted!!",
-    "冰淇淋 冰淇淋 冰淇淋 冰淇淋 冰淇淋",
-    "This is a post",
-    "Sometimes I think but then i stop"].sort(() => Math.random() - 0.5));
+/**
+ * @type {string[]}
+ */
+const emptyArr = [];
+const [getPosts, setPosts] = createSignal(emptyArr);
 
 
 const InfoColumn = () => {
@@ -88,7 +83,7 @@ const PostsColumn = () => {
                 ${(/** @type {string} */ item) => html`<${Post} postText="${item}"><//>`}
             <//>
         </div>
-    `
+    `;
 };
 
 const App = () => {
